df25ce20b97943e7cfc7554197263e757af0ac22
陈东方 <2873044836@qq.com>
2026-06-20 03:13:43 +0800

    更新Steam链接为个人主页

--- src/config.ts
+++ src/config.ts
@@ -83,7 +83,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "Steam",
 			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			url: "https://steamcommunity.com/profiles/76561198859414380/",
 		},
 	],
 };
